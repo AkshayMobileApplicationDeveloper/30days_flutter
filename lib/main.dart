9c6a7fab528146f508bc064406717d444c21ef2f
AkshayMobileApplicationDeveloper <akshaykumarprajapati@hotmail.com>
2024-06-11 13:10:13 +0530

    day 04

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
-void main() {
+import 'package:flutter/material.dart';
+import 'package:my_app/home_page.dart';
+import 'package:my_app/login_pages.dart';
 
-    // ignore: avoid_print
-    print("object");
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage(),
+      },
+    );
   }
+}
 
